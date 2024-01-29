adecd365c54208696815834fa29d8f13998b0e89
HaolanDuan <duanhaolan@gmail.com>
2024-01-29 21:37:57 +0800

    jan29

--- proj1a/src/LinkedListDeque.java
+++ proj1a/src/LinkedListDeque.java
@@ -1,6 +1,16 @@
 import java.util.List;
 
 public class LinkedListDeque<T> implements Deque<T> {
+    public class Node {
+        public int item;
+        public Node next;
+        public Node before;
+        public Node(int i, Node n, Node b) {
+            item = i;
+            next = n;
+            before = b;
+        }
+    }
     @Override
     public void addFirst(T x) {
 
@@ -45,8 +55,7 @@ public class LinkedListDeque<T> implements Deque<T> {
     public T getRecursive(int index) {
         return null;
     }
-
     public LinkedListDeque() {
+        
     }
 }
-
